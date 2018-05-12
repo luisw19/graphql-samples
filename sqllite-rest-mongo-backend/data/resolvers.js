1d47fd637e6cc60b61b0b074198ccead413592bf
luisw19 <luisw19@gmail.com>
2018-05-12 13:21:30 +0100

    added useful tip on how to deal with resolvers in line 28 and 32

--- sqllite-rest-mongo-backend/data/resolvers.js
+++ sqllite-rest-mongo-backend/data/resolvers.js
@@ -10,12 +10,14 @@ const resolvers = {
   Query: {
     //operation to get an Author
     author(_, args) {
+      console.log("Query.author" + " with: " + JSON.stringify(args));
       return Author.find({
         where: args
       });
     },
     //operation to get all Authors
     allAuthors(_, args) {
+      console.log("Query.allAuthors");
       return Author.findAll();
     },
     //operation to get random fortue cookie by fetching fortune cookie REST API
@@ -23,10 +25,19 @@ const resolvers = {
       return FortuneCookie.getOne();
     }
   },
-  //Author Object Type
+  //Resolver for Author Object Type. Each field can have a resolver and this will take precedence over resolver in the Query type.
   Author: {
+    //Uncomment and see how values for firstName and lastName take precedence over those obtained in the resolver for author Query
+    firstName(_){
+      // The underscore (_) argument is the result of previous parent resolver using Author type, in this case author, or AllAuthors
+      return _.firstName + " -overridden";
+    },
+    lastName(_){
+      return _.lastName + " -overridden";
+    },
     //post field
     posts(author) {
+      console.log("Author.posts called");
       return author.getPosts();
     }
   },
@@ -38,11 +49,14 @@ const resolvers = {
     },
     //views field
     views(post) {
+      console.log("Post.views called");
       return View.findOne({
         postId: post.id
       }).then(view => view.views);
     }
   }
 };
+
+
 
 export default resolvers;
