9a613f8d30b770529d6e6ea6c436fd763acdbf5c
luisw19 <luisw19@gmail.com>
2018-05-12 13:21:58 +0100

    added useful tip on how to deal with resolvers in line 28 and 32

--- sqllite-rest-mongo-backend/data/resolvers.js
+++ sqllite-rest-mongo-backend/data/resolvers.js
@@ -28,13 +28,13 @@ const resolvers = {
   //Resolver for Author Object Type. Each field can have a resolver and this will take precedence over resolver in the Query type.
   Author: {
     //Uncomment and see how values for firstName and lastName take precedence over those obtained in the resolver for author Query
-    firstName(_){
+    /*firstName(_){
       // The underscore (_) argument is the result of previous parent resolver using Author type, in this case author, or AllAuthors
       return _.firstName + " -overridden";
     },
     lastName(_){
       return _.lastName + " -overridden";
-    },
+    },*/
     //post field
     posts(author) {
       console.log("Author.posts called");
